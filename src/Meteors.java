b060ab8cc5c42bc56a7c689d5aedc2236b9653d1
Robert Ciliberto <rciliberto2@bostonk12.org>
2017-05-30 16:27:27 -0400

    Increasing difficulty as time goes on and beginnings of a score count

--- src/Meteors.java
+++ src/Meteors.java
@@ -16,6 +16,9 @@ public class Meteors extends PApplet{
 	ArrayList<Meteor> meteors;
 	ArrayList<Bullet> bullets;
 	ArrayList<StarLine> starLines;
+	boolean paused;
+	int score=0;
+	int highScore=0;
 	
 	public void settings(){
 		size(1080,720,P3D);
@@ -24,6 +27,8 @@ public class Meteors extends PApplet{
 	public void setup(){
 		frameRate(60);
 		
+		paused=false;
+		score=0;
 		ship=new Spaceship(this);
 		meteors=new ArrayList<Meteor>();
 		bullets=new ArrayList<Bullet>();
@@ -31,23 +36,49 @@ public class Meteors extends PApplet{
 	}
 	
 	public void draw(){
-		background(0);
-		lights();
-		createMeteors();
-		createStarLines();
-		
-		//Render Ship
-		ship.render();
-		
-		cycleMeteors();
-		renderStarLines();
-		cycleBullets();
+		if(!paused){
+			background(0);
+			lights();
+			createMeteors();
+			createStarLines();
+			
+			//Render Ship
+			ship.render();
+			
+			cycleMeteors();
+			renderStarLines();
+			cycleBullets();
+		}
 	}
 	
-	//Adds a new Meteor object to the ArrayList every second
+	//Adds a new Meteor object to the ArrayList
 	public void createMeteors(){
-		if(frameCount%60==0)
-			meteors.add(new Meteor(this, random(50,width-50), random(50,height-50)));
+		if(frameCount<900){
+			if(frameCount%60==0){
+				meteors.add(new Meteor(this, random(50,width-50), random(50,height-50)));
+			}
+		}
+		else if(frameCount<1800){
+			if(frameCount%45==0){
+				meteors.add(new Meteor(this, random(50,width-50), random(50,height-50)));
+			}
+		}
+		else if(frameCount<2700){
+			if(frameCount%30==0){
+				meteors.add(new Meteor(this, random(50,width-50), random(50,height-50)));
+			}
+		}
+		else{
+			if(frameCount%30==0){
+				meteors.add(new Meteor(this, random(50,width-50), random(50,height-50)));
+			}
+		}
+	}
+	
+	//Removes meteor and adds to the score
+	public void removeMeteor(int index){
+		meteors.remove(index);
+		score+=10;
 	}
 	
 	//Adds a new StarLine object to the ArrayList
@@ -100,7 +131,7 @@ public class Meteors extends PApplet{
 			for(int j=0; j<meteors.size(); j++){
 				if(size==bullets.size()){
 					if(bullet.checkCollision(meteors.get(j))){
-						meteors.remove(j);
+						removeMeteor(j);
 						j--;
 						bullets.remove(i);
 					}
@@ -115,17 +146,25 @@ public class Meteors extends PApplet{
 	}
 
 	public void keyPressed(){
-		if(keyCode==UP || key=='w') ship.moveUp();
-		if(keyCode==DOWN || key=='s') ship.moveDown();
-		if(keyCode==LEFT || key=='a') ship.moveLeft();
-		if(keyCode==RIGHT || key=='d') ship.moveRight();
-		if(keyCode==ENTER || key==' ') fireBullet();  
+		if(!paused){
+			if(keyCode==UP || key=='w') ship.moveUp();
+			if(keyCode==DOWN || key=='s') ship.moveDown();
+			if(keyCode==LEFT || key=='a') ship.moveLeft();
+			if(keyCode==RIGHT || key=='d') ship.moveRight();
+			if(keyCode==ENTER || key==' ') fireBullet(); 
+		}
 	}
 	
 	public void keyReleased(){
-		if(keyCode==UP || key=='w') ship.haltUp();
-		if(keyCode==DOWN || key=='s') ship.haltDown();
-		if(keyCode==LEFT || key=='a') ship.haltLeft();
-		if(keyCode==RIGHT || key=='d') ship.haltRight();
+		if(!paused){
+			if(keyCode==UP || key=='w') ship.haltUp();
+			if(keyCode==DOWN || key=='s') ship.haltDown();
+			if(keyCode==LEFT || key=='a') ship.haltLeft();
+			if(keyCode==RIGHT || key=='d') ship.haltRight();
+		}
+	}
+	
+	public void mousePressed(){
+		if(!paused) fireBullet();
 	}
 }
